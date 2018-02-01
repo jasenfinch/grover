895abeb6ea1652caf69af352e18ac2ddba615c33
jasenfinch <jsf9@aber.ac.uk>
2018-02-01 17:30:30 +0000

    added tidyup

--- api.R
+++ api.R
@@ -20,4 +20,12 @@ convertFile <- function(authKey,instrument,directory,file,args='',res){
   }
 }
 
-
+#* @get /tidyup
+removeDirectory <- function(authKey,instrument,directory){
+  key <- readLines('~/grover.txt')[3]
+  if (authKey == key){
+    unlink(str_c('C:/TMP_STORE/',instrument,'/',directory),recursive = T)
+  } else {
+    stop('Incorrect authentication key')
+  }
+}
