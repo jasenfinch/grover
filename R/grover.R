857aa3a28dfe0b5bf4e5bf4cc79c72c52a4c7563
jasenfinch <jsf9@aber.ac.uk>
2018-10-29 17:35:03 +0000

    added checkGrover()

--- R/grover.R
+++ R/grover.R
@@ -6,6 +6,7 @@ readGrover <- function(path){
   grover(details[[1]],details[[2]],details[[3]])
 }
 
+#' grover
 #' @export
 
 grover <- function(host,port,auth){
@@ -15,20 +16,38 @@ new('Grover',
     auth = auth)
 }
 
+#' host
 #' @export
 
 host <- function(grove){
   grove@host
 }
 
+#' port
 #' @export
 
 port <- function(grove){
   grove@port
 }
 
+#' auth
 #' @export
 
 auth <- function(grove){
   grove@auth
+}
+
+#' checkGrover
+#' @export
+
+checkGrover <- function(grove){
+  cmd <- str_c(host(grove),':',port(grove),'/extant?','authKey=',auth(grove))
+  answer <- try({cmd %>%
+      GET() %>%
+      content() %>%
+      unlist()},silent = T)
+  if (answer != "I'm still here!") {
+    answer <- 'grover is MIA!'
+  }
+  return(answer)
 }
