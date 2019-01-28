f6ffb3fe8071f92811d6ca0bd72e4d1ce5af804a
jasenfinch <jsf9@aber.ac.uk>
2019-01-28 21:35:31 +0000

    Grover class now exported and added documentation. Changed version to 0.1.2

--- R/allClasses.R
+++ R/allClasses.R
@@ -1,3 +1,10 @@
+#' Grover
+#' @rdname Grover-class
+#' @description A simple S4 class containing grover API host information
+#' @slot host host address
+#' @slot port port on which the API is hosted
+#' @slot auth authentication key
+#' @export
 
 setClass('Grover',
          slots = list(
