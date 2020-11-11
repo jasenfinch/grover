84650a70a795bd734f93c76103167c58937db3ea
jasenfinch <jsf9@aber.ac.uk>
2020-11-11 12:20:49 +0000

    removed rawFile

--- R/client-transfer.R
+++ R/client-transfer.R
@@ -46,45 +46,6 @@ setMethod('getFile',signature = 'GroverClient',
             }
           })
 
-#' rawFile
-#' @description Transfer a raw MS file using the grover API.
-#' @param grove S4 object of class Grover
-#' @param instrument instrument name
-#' @param directory directory name
-#' @param file file name
-#' @param outDir output directory path for converted files
-#' @export
-
-rawFile <- function(grove,instrument,directory,file,outDir = '.'){
-  cat('\n',file,' ',cli::symbol$continue,'\r',sep = '')
-  cmd <- str_c(hostURL(grove),'/getRaw?',
-               'authKey=',auth(grove),
-               '&instrument=',instrument,
-               '&directory=',directory,
-               '&file=',file)
-  rawData <- cmd %>%
-    URLencode() %>%
-    GET()
-  if (rawData$status_code == 200) {
-    con <- str_c(outDir,'/',file) %>%
-      file('wb')
-    rawD <- rawData %>%
-      content(as = 'text',encoding = 'UTF-8') %>%
-      base64enc::base64decode()
-    writeBin(rawD,con)
-    close(con)
-    success <- 1
-  } else {
-    success <- 0
-  }
-  if (success == 1) {
-    cat('\r',file,' ',crayon::green(cli::symbol$tick),'\n',sep = '')
-  }
-  if (success == 0) {
-    cat('\r',file,' ',crayon::red(cli::symbol$cross),'\n',sep = '')
-  }
-}
-
 #' rawDirectory
 #' @description Transfer a directory of raw files using the grover API.
 #' @param grove S4 object of class Grover
