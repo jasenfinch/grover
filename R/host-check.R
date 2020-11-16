be9deb974af9e35a8e8d224cc3631da2d263cbc8
jasenfinch <jsf9@aber.ac.uk>
2020-11-16 23:59:43 +0000

    check now works in background api

--- R/host-check.R
+++ R/host-check.R
@@ -1,10 +1,15 @@
 
 hostExtant <- function(auth){
   
-  grover_host <- readGrover(groverHostTemp())
-  host_auth <- auth(grover_host)
+  grover_host <- yaml::read_yaml(
+    stringr::str_c(tempdir(),
+                   'grover_host.yml',
+                   sep = '/'))
+  host_auth <- grover_host$auth
   
-  checkAuth(auth,host_auth)
+  if (auth != host_auth){
+    stop('Incorrect authentication key',call. = FALSE)
+  }
   
   return("I'm still here!")
 }
--- R/host-internals.R
+++ R/host-internals.R
@@ -35,27 +35,28 @@ checkAuth <- function(auth,host_auth){
   }
 }
 
-convert_empty <- function(string) {
-  if (string == "") {
-    "-"
-  } else {
-    string
-  }
-}
-
 host_preroute <-  function(req) {
-  tic()
+  tictoc::tic()
 }
 
 host_postroute <- function(req, res) {
-  end <- toc(quiet = TRUE)
+  
+  convert_empty <- function(string) {
+    if (string == "") {
+      "-"
+    } else {
+      string
+    }
+  }
+  
+  end <- tictoc::toc(quiet = TRUE)
   
   print (req)
   
-  log_info(str_c(
+  logger::log_info(stringr::str_c(
     convert_empty(req$REQUEST_METHOD),
     convert_empty(req$PATH_INFO),
-    str_c({req$args},collapse = ";"),
+    stringr::str_c({req$args},collapse = ";"),
     convert_empty(res$status),
     round(end$toc - end$tic, digits = getOption("digits", 5)),
     sep = ' '
