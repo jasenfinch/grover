6faf278ac3277e3d49386694a548bca0c0696ba0
jasenfinch <jsf9@aber.ac.uk>
2020-11-17 00:43:48 +0000

    file transfer now works in background API

--- R/host-transfer.R
+++ R/host-transfer.R
@@ -2,18 +2,24 @@
 
 hostGetFile <- function(auth,instrument,directory,file){
   
-  grover_host <- readGrover(groverHostTemp())
-  host_auth <- auth(grover_host)
-  host_repository <- repository(grover_host)
+  grover_host <-  yaml::read_yaml(
+    stringr::str_c(tempdir(),
+                   'grover_host.yml',
+                   sep = '/'))
   
-  checkAuth(auth,host_auth)
+  host_auth <- grover_host$auth
+  host_repository <- grover_host$repository
   
-  f <- str_c(host_repository,instrument,directory,file,sep = '/')
-  if (file_exists(f)) {
+  if (auth != host_auth){
+    stop('Incorrect authentication key',call. = FALSE)
+  } 
+  
+  f <- stringr::str_c(host_repository,instrument,directory,file,sep = '/')
+  if (fs::file_exists(f)) {
     con <- file(f,'rb')
     f <- readBin(con,
                  'raw',
-                 n = file_info(f)$size) 
+                 n = fs::file_info(f)$size) 
     close(con)
   } else {
     stop('File not found!')
