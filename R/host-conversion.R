06440f54b1b569437fd594b391e282f39c60639b
jasenfinch <jsf9@aber.ac.uk>
2020-11-17 00:22:20 +0000

    conversion now works in background

--- R/host-conversion.R
+++ R/host-conversion.R
@@ -2,15 +2,20 @@
 
 hostConvertFile <- function(auth,instrument,directory,file,args = ''){
   
-  grover_host <- readGrover(groverHostTemp())
-  host_auth <- auth(grover_host)
-  host_repository <- repository(grover_host)
+  grover_host <-  yaml::read_yaml(
+    stringr::str_c(tempdir(),
+                   'grover_host.yml',
+                   sep = '/'))
+  host_auth <- grover_host$auth
+  host_repository <- grover_host$repository
   
-  checkAuth(auth,host_auth)
+  if (auth != host_auth){
+    stop('Incorrect authentication key',call. = FALSE)
+  }
   
   tmp_dir <- tempdir()
   
-  convert_files(files = str_c(host_repository,
+  msconverteR::convert_files(files = stringr::str_c(host_repository,
                               instrument,
                               directory,
                               file,
@@ -18,8 +23,8 @@ hostConvertFile <- function(auth,instrument,directory,file,args = ''){
                 outpath = tmp_dir,
                 args = args)
   
-  out_file <- str_c(tmp_dir,
-                    str_c(tools::file_path_sans_ext(file),
+  out_file <- stringr::str_c(tmp_dir,
+                    stringr::str_c(tools::file_path_sans_ext(file),
                           '.mzML'),
                     sep = '/')
   
--- R/host-tidy.R
+++ R/host-tidy.R
@@ -1,14 +1,20 @@
 
 hostTidy <- function(auth,file){
   
-  grover_host <- readGrover(groverHostTemp())
-  host_auth <- auth(grover_host)
+  grover_host <-  yaml::read_yaml(
+    stringr::str_c(tempdir(),
+                   'grover_host.yml',
+                   sep = '/'))
+  host_auth <- grover_host$auth
   
-  checkAuth(auth,host_auth)
+  if (auth != host_auth){
+    stop('Incorrect authentication key',call. = FALSE)
+  }
   
   tmp_dir <- tempdir()
   
-  file_delete(str_c(tmp_dir,file,sep = '/')) %>%
-    path_file()
+  fd <- fs::file_delete(stringr::str_c(tmp_dir,file,sep = '/'))
+  fd <-  fs::path_file(fd)
   
+  return(fd)
 }
