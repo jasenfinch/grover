8606e21f76f8d78d817dd98070ed21657c74a9db
jasenfinch <jsf9@aber.ac.uk>
2022-11-17 08:08:40 +0000

    test logs directory now specified in tempdir(). Fixed extant() test

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -13,7 +13,9 @@ grover_client <- grover(host = "127.0.0.1",
                        port = 8000,
                        auth = "1234")
 
-api <- groverAPI(grover_host,background = TRUE)
+api <- groverAPI(grover_host,
+                 background = TRUE,
+                 log_dir = paste0(tempdir(),'/logs'))
 
 Sys.sleep(8)
 
@@ -22,7 +24,7 @@ test_that('api is running',{
 })
 
 test_that('client can detect api',{
-  expect_equal(extant(grover_client),"I'm still here!")
+  expect_true(extant(grover_client))
 })
 
 test_that('the api version is returned',{
